182e70e8366022d7098a2458cb2fb24c09d86b91
Edison Ong <e4ong1031@gmail.com>
2015-10-02 16:58:16 -0400

    Added maintenance script for RDF update.

---
+++ script/Maintenance.php
@@ -0,0 +1,68 @@
+<?php
+
+/**
+ * Copyright © 2015 The Regents of the University of Michigan
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ * 
+ * For more information, questions, or permission requests, please contact:
+ * Yongqun “Oliver” He - yongqunh@med.umich.edu
+ * Unit for Laboratory Animal Medicine, Center for Computational Medicine & Bioinformatics
+ * University of Michigan, Ann Arbor, MI 48109, USA
+ * He Group:  http://www.hegroup.org
+ */
+
+/**
+ * @file Api.php
+ * @author Edison Ong
+ * @since Oct 1, 2015
+ * @comment 
+ */
+
+use PDO;
+ 
+abstract class Maintenance {
+	protected $db;
+	
+	public function setup() {
+		$tokens = explode( DIRECTORY_SEPARATOR, __DIR__ );
+		$dir = implode( DIRECTORY_SEPARATOR, array_splice( $tokens, 0, -1 ) );
+		DEFINE( 'SCRIPTPATH',  $dir . DIRECTORY_SEPARATOR );
+		DEFINE( 'APPPATH', SCRIPTPATH . 'application' . DIRECTORY_SEPARATOR );
+		DEFINE ( 'TMP', SCRIPTPATH . 'tmp' . DIRECTORY_SEPARATOR );
+		require APPPATH . 'Config/DB.php';
+	}
+	
+	protected function openPDOConnection() {
+		$options = array(
+			PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_OBJ,
+			PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING,
+		);
+		$this->db = new PDO( DB_DRIVER . ':host=' . DB_HOST . ';dbname=' . DB_SCHEMA, DB_USERNAME, DB_PASSWORD, $options );
+	}
+	
+	protected function getFinalURL( $url ) {
+		$request = curl_init ( $url );
+		curl_setopt ( $request, CURLOPT_FOLLOWLOCATION, true );
+		curl_setopt ( $request, CURLOPT_RETURNTRANSFER, true );
+	
+		curl_exec ( $request );
+	
+		if ( !curl_errno ( $request ) )
+			$url = curl_getinfo ($request, CURLINFO_EFFECTIVE_URL );
+	
+		curl_close ( $request );
+	
+		return $url;
+	}
+}
+
+?>
---
+++ script/UpdateOntology.php
@@ -0,0 +1,235 @@
+<?php
+
+/**
+ * Copyright © 2015 The Regents of the University of Michigan
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and limitations under the License.
+ * 
+ * For more information, questions, or permission requests, please contact:
+ * Yongqun “Oliver” He - yongqunh@med.umich.edu
+ * Unit for Laboratory Animal Medicine, Center for Computational Medicine & Bioinformatics
+ * University of Michigan, Ann Arbor, MI 48109, USA
+ * He Group:  http://www.hegroup.org
+ */
+
+/**
+ * @file UpdateOntology.php
+ * @author Edison Ong
+ * @since Oct 1, 2015
+ * @comment 
+ */
+
+if (PHP_SAPI == 'cli' ) {
+	require( 'Maintenance.php' );
+}
+
+use Exception;
+use PDO;
+
+Class UpdateOntology extends Maintenance {
+	private $tmpDir;
+	private $ontology;
+	
+	public $fileName;
+	public $file;
+	
+	public function __construct( $ontAbbr, $options ) {
+		$this->setup();
+		$this->openPDOConnection();
+		
+		$this->tmpDir = TMP . 'rdf' . DIRECTORY_SEPARATOR;
+		mkdir( $this->tmpDir );
+		
+		$sql = "SELECT * FROM ontology WHERE ontology_abbrv = '$ontAbbr'";
+		$query = $this->db->prepare( $sql );
+		$query->execute();
+		$this->ontology = $query->fetch();
+		$this->fileName = $this->ontology->ontology_abbrv;
+	}
+	
+	public function doUpdate() {
+		
+		if ( $this->ontology->download != '' ) {
+			$this->download( $this->ontology->download );
+		}
+		
+		if ( !file_exists( $this->file ) && $this->ontology->alternative_download != '' ) {
+			$this->download( $this->ontology->alternative_download );
+		}
+		
+		if ( !file_exists( $this->file ) && $this->ontology->source != '' ) {
+			$this->download( $this->ontology->source );
+		}
+		
+		if ( !file_exists( $this->file ) ) {
+			echo "Failed to download owl file.\n";
+		} else {
+			$md5 =  md5_file( $this->file );
+			
+			if ( $md5 == $this->ontology->md5 && $this->ontology->loaded == 'y' ) {
+				echo "Ontology already up-to-date.\n";
+			} else {
+				$sql = "UPDATE ontology SET loaded='n' where id = '{$this->ontology->id}'";
+				$this->db->query( $sql );
+				
+				$cmd =
+<<<END
+/data/usr/local/virtuoso/bin/isql 1111 dba dJay0D2a verbose=on banner=off prompt=off echo=ON errors=stdout exec="log_enable(3,1);
+sparql clear graph <{$this->ontology->ontology_graph_url}>;
+DB.DBA.RDF_LOAD_RDFXML_MT (file_to_string_output ('$this->file'), '', '{$this->ontology->ontology_graph_url}');"
+END;
+				exec( $cmd, $output);
+				$output = join( "\n", $output );
+				$sql = "UPDATE ontofox.ontology SET log=" . $this->db->quote( $output ) . " where id = '{$this->ontology->id}'";
+				$this->db->query( $sql );
+				
+				if ( !preg_match( '/Error/', $output ) ) {
+					$sql = "UPDATE ontology SET loaded='y', md5='$md5', last_update=now() where id = '{$this->ontology->id}'";
+					$this->db->query( $sql );
+					echo "$this->fileName loaded\n";
+				}
+				else {
+					echo "$this->fileName failed\n";
+				}
+				#$this->remove( $this->tmpDir );
+				
+				#TODO: Special treatment of VO
+				/*
+				if ($id=='vaccine') {
+					$reasoned_file_name = "$tmp_dir/$id"."_reason.owl";
+					if(file_exists($reasoned_file_name)) unlink($reasoned_file_name);
+					system("java -Xmx8g -cp .:./libs/* org.hegroup.rdfstore.OWLReason $file_name $reasoned_file_name");
+					if (file_exists($reasoned_file_name)) {
+						$reasoned_graph_url=str_replace('/merged/', '/inferred/', $graph_url);
+				
+						exec('/data/usr/local/virtuoso/bin/isql 1111 dba dJay0D2a verbose=on banner=off prompt=off echo=ON errors=stdout exec="log_enable(3,1); sparql clear graph <'.$reasoned_graph_url.'>; DB.DBA.RDF_LOAD_RDFXML_MT (file_to_string_output (\''.$reasoned_file_name.'\'), \'\', \''.$reasoned_graph_url.'\');"', $output);
+					}
+				}
+				*/
+			}
+		}
+	}
+	
+	private function remove( $dir ) {
+		$it = new RecursiveDirectoryIterator( $dir, RecursiveDirectoryIterator::SKIP_DOTS );
+		$files = new RecursiveIteratorIterator( $it, RecursiveIteratorIterator::CHILD_FIRST );
+		foreach( $files as $file ) {
+			if ( $file->isDir() ){
+				rmdir( $file->getRealPath() );
+			} else {
+				unlink( $file->getRealPath() );
+			}
+		}
+		rmdir( $dir );
+	}
+	
+	private function download( $input ) {
+		$tokens = explode( '|', $input );
+		if ( sizeof( $tokens ) > 1 ) {
+			$downloadURL = array_pop( $tokens );
+		} else {
+			$downloadURL = array_shift( $tokens );
+		}
+		
+		echo "$this->fileName: getting final url $downloadURL\n";
+		$downloadURL = $this->getFinalURL( $downloadURL );
+		
+		echo "$this->fileName: loading data from $downloadURL\n";
+		if ( preg_match( '/\.zip/', $downloadURL ) != false ) {
+			exec( "wget $downloadURL -O $this->tmpDir$this->fileName.zip" );
+			$this->decompress( 'zip' );
+		} else {
+			exec( "wget -q $downloadURL -O $this->tmpDir$this->fileName.owl" );
+		}
+		if ( $this->ontology->do_merge == 'y' ) {
+			$this->merge( $downloadURL );
+		} else {
+			if ( file_exists( "$this->tmpDir$this->fileName.owl" ) ) {
+				$this->file = "$this->tmpDir$this->fileName.owl";
+			}
+		}
+	}
+	
+# Need to support other compress format
+	private function decompress( $format ) {
+		switch ( $format ) {
+			case 'zip':
+				exec( "unzip $this->tmpDir$this->fileName.zip -d $this->tmpDir$this->fileName");
+				exec( "unzip -l $this->tmpDir$this->fileName.zip | grep .owl | awk '{cmd=\"mv $this->tmpDir$this->fileName" .
+					DIRECTORY_SEPARATOR .
+					"\" $4 \" $this->fileName.owl\"; system(cmd); close(cmd);}");
+				break;
+		}
+		unlink( "$this->tmpDir$this->fileName" );
+	}
+	
+	private function merge( $downloadURL ) {
+		$settings = array();
+		$settings['download_url'] = $downloadURL;
+		$settings['output_file'] = "$this->tmpDir$this->fileName.merged.owl";
+		
+		$settings['mapping'] = $this->map( $downloadURL, "$this->tmpDir/$this->fileName.mapping" );
+		
+		file_put_contents( "$this->tmpDir/$this->fileName.json", json_encode( $settings ) );
+		
+		$importDir = SCRIPTPATH . 'ontology' . DIRECTORY_SEPARATOR;
+		exec( 'java -Xmx8g -cp "' . SCRIPTPATH . "library/java/*\" org.hegroup.owlmerge.OWLMerge $this->tmpDir/$this->fileName.json $importDir" );
+		if ( file_exists( $settings['output_file'] ) ) {
+			$this->file = $settings['output_file'];
+		}
+	}
+	
+	private function map( $downloadURL, $output ) {
+		$mappings = array();
+		$position = strrpos( $downloadURL, '/' );
+		if ( $position != false ) {
+			$strFolder = substr( $downloadURL, 0, $position );
+			exec( "wget -q $strFolder/catalog-v001.xml -O $output" );
+			$mappingContent = file_get_contents( $output );
+			
+			preg_match_all( '/<uri id="[^"]+" name="([^"]+)" uri="([^"]+)"\/>/', $mappingContent, $matches, PREG_SET_ORDER );
+			foreach( $matches as $match ) {
+				$mapping['to'] = $match[1];
+				$mapping['from'] = $strFolder . '/' . $match[2];
+				$mappings[] = $mapping;
+			}
+		}
+		return $mappings;
+	}
+	
+}
+
+if (PHP_SAPI == 'cli' ) {
+	if ( sizeof( $argv ) > 1 ) {
+		$args = $argv;
+		unset( $args[0] );
+		$args = array_values( $args );
+		$ontAbbr = $args[0];
+		unset( $args[0] );
+		$args = array_values( $args );
+		$options = array();
+		if ( sizeof( $args ) > 0 ) {
+			if ( sizeof( $args ) % 2 == 0 ) {
+				for ( $i = 0; $i < sizeof( $args ) / 2; $i++ ) {
+					$options[$args[$i]] = $args[$i+1];
+				}
+			} else {
+				throw new Exception( 'Invalid arguments.' );
+			}
+		}
+	} else {
+		throw new Exception( 'Invalid arguments.' );
+	}
+	$update = new UpdateOntology( $ontAbbr, $options );
+	$update->doUpdate();
+}
+
+?>
