37d899560742a2ac50a4e2cade72fd7ac4a30bee
Unknown <e4ong1031@gmail.com>
2018-07-10 13:05:33 -0400

    Updated highlight.
    Fixed whole word matching.
    Support words separated by '-'.

--- public/js/annotator.display.js
+++ public/js/annotator.display.js
@@ -6,7 +6,13 @@ $( function() {
 	$( 'tr.highlight' ).hover( function( event ) {
 		var term = $( this ).find( ".term" ).text();
 		console.log( term );
-		$( "p.querytext" ).mark( term );
+		$( "p.querytext" ).mark( term, {
+			"separateWordSearch":false,
+			"accuracy":{
+				"value":"exactly",
+				"limiters":["-",",","."]
+			}
+			});
 	}, function() {
 		$( "p.querytext" ).unmark();
   });
